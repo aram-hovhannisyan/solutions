92a23d963ee13bbc9049816b8d8952a7581fe365
aramhovhannisyan02 <115581991+aramhovhannisyan02@users.noreply.github.com>
2022-11-05 18:50:42 +0400

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,16 @@
+# num = int(input('Enter number: '))
+# y = num // 3
+# x = ((2*(y-1) - (y-2))*(y-1))/2
+# while x + y > num:
+#     y = y - 1
+#     x = ((2*(y-1) - (y-2))*(y-1))/2
+# z = y
+# res = -z + 1
+# for i in range(1,y + 1):
+#     res = -z + 1
+#     z = y
+#     for j in range(i):
+#         res = res + z 
+#         z = z - 1
+#         print(res, end = '  ')
+#     print('\n')
